7743c0988b56f1c3b50c05ec5446c097af7e883e
Nick Strayer <nstrayer@uvm.edu>
2015-07-19 18:26:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//just some nonsense to move objects to front easily.
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+
+var width = parseInt(d3.select("#viz").style("width").slice(0, -2)),
+    height = $(window).height() - 85,
